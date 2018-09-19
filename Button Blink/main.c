1ef0a5f09eb47862f0a08a3a6a08c71bd5d491be
unknown <mclemoret0@students.rowan.edu>
2018-09-19 13:29:46 -0400

    ButtonBlink: Added Basic functons

---
+++ Button Blink/main.c
@@ -0,0 +1,31 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+        P1DIR |= 0x65;                          //set P1.0 and P1.6 as an output. Use "|=" so that it does not overwrite unnecessary bits
+                                                   //^This also means that P1.3 is an input^
+
+        volatile unsigned int i;                //This will not be optimized and removed by the compiler
+
+        while(true){                            //Infinite while loop
+            if(time % 5000 == 0){
+                P1OUT ^= BIT0;
+                if(time > 7000){
+                    time = 3000;
+                }
+            }
+            if(time % 7000 == 0){
+                P1OUT ^= BIT6;
+            }
+            time ++;
+
+            //P1OUT ^= 0x01;                      //Flip P1OUT to be the opposite of what it was before
+            //for(i = 0; i < 20000; i++);         //A for loop that counts up, basically a delay that occupies CPU cycles
+}
+
+}
