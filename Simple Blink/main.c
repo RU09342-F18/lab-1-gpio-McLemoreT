fd1972e18d6ab797e94fac08e0ef924178c39b35
unknown <mclemoret0@students.rowan.edu>
2018-09-19 13:22:30 -0400

    SimpleBlink: Fixed comment spelling error

--- Simple Blink/main.c
+++ Simple Blink/main.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	            // stop watchdog timer
-	P1DIR |= 0x01;                          //set P1.0 as an output. Use "|=" so that it does not overwrite unessesary bits
+	P1DIR |= 0x01;                          //set P1.0 as an output. Use "|=" so that it does not overwrite unnecessary bits
 	volatile unsigned int i;                //This will not be optimized and removed by the compiler
-	while(true){                            //Infinite while loop
-	    P1OUT ^= 0x01;                      //Flip P1OUT to be the oposite of what it was before
-	    for(i = 0; i < 20000; i++);         //A for loop that counts up, basically a delay that occupis CPU cycles
+	while(1){                            //Infinite while loop
+	    P1OUT ^= 0x01;                      //Flip P1OUT to be the opposite of what it was before
+	    for(i = 0; i < 20000; i++);         //A for loop that counts up, basically a delay that occupies CPU cycles
 	}
 }
