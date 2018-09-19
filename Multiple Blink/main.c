cfa492a1d3a8d58669abc9bbdb6fab7f554b6d8b
unknown <mclemoret0@students.rowan.edu>
2018-09-19 12:01:39 -0400

    Better spacing

--- Multiple Blink/main.c
+++ Multiple Blink/main.c
@@ -9,6 +9,7 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	    P1DIR |= 0x01;                          //set P1.0 as an output. Use "|=" so that it does not overwrite unessesary bits
 	    volatile unsigned int i;                //This will not be optimized and removed by the compiler
+
 	    while(true){                            //Infinite while loop
 	        P1OUT ^= 0x01;                      //Flip P1OUT to be the oposite of what it was before
 	        for(i = 0; i < 20000; i++);         //A for loop that counts up, basically a delay that occupis CPU cycles
