3de0b9e17908bce678c6ae683540463c8adc6984
unknown <mclemoret0@students.rowan.edu>
2018-09-21 19:08:02 -0400

    OffBoard: Added same comments to Off_Board

--- Off_Board Blink/main.c
+++ Off_Board Blink/main.c
@@ -14,24 +14,24 @@ typedef int bool;
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    bool i = 0;
+    bool i = 0;                 //create a boolean called "i"
 
-    P1SEL &= (~LED & ~Btn);
+    P1SEL &= (~LED & ~Btn);     //On P1 Select, set bits "0" and "3" to the value "0"
 
-    P1DIR |= LED;
-    P1OUT &= ~LED;
+    P1DIR |= LED;               //Set Port 1.0 as an output
+    P1OUT &= ~LED;              //Set the initial value of port 1.0 as "0"
 
-    P1DIR &= ~Btn;
-    P1REN |= Btn;
-    P1OUT |= Btn;
+    P1DIR &= ~Btn;              //Set Port 1.3 as an input
+    P1REN |= Btn;               //Turn on the pull-up resistor for port 1.3
+    P1OUT |= Btn;               //Set the initial value of port 1.3 as "1"
 
-    while(1){
-        if(!PnB && !i){
-                    P1OUT ^= LED;
-                    i = 1;
+    while(1){                   //Start an infinite while loop.
+        if(!PnB && !i){         //If the button is pushed and the boolean is false
+                    P1OUT ^= LED;   //Flip the value of the LED
+                    i = 1;          //Flip the Value of the Boolean
         }
-        else if(PnB && i){
-                   i = 0;
+        else if(PnB && i){          //If the button is pushed and the boolean is true
+                   i = 0;           //Set the Boolean to be true
        }
 
     }
