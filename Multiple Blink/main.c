ad6f722f002ddfdd0c71f2c367724881fc683a32
unknown <mclemoret0@students.rowan.edu>
2018-09-21 18:55:25 -0400

    Multi: Added comments

--- Multiple Blink/main.c
+++ Multiple Blink/main.c
@@ -13,14 +13,14 @@ int main(void)
 	    volatile unsigned int time = 0;         //This will not be optimized and removed by the compiler
 
 	    while(1){                            //Infinite while loop
-	        if(time % 5000 == 0){
-	            P1OUT ^= BIT0;
+	        if(time % 5000 == 0){           //Test if the current value of time is divisible by 5000
+	            P1OUT ^= BIT0;              //If time is divisible by 5000 then flip the value of BIT0 (LED 0)
 	            if(time > 7000){
 	                //time = 3000;
 	            }
 	        }
-	        if(time % 7000 == 0){
-	            P1OUT ^= BIT6;
+	        if(time % 7000 == 0){           //Test if the current value of time is divisible by 7000
+	            P1OUT ^= BIT6;              //If time is divisible by 7000 then flip the value of BIT6 (LED 6)
 	        }
 	        time ++;
 
