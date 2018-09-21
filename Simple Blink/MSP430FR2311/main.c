3d5f5ffa65c95249276293d1b94be727c621e580
unknown <mclemoret0@students.rowan.edu>
2018-09-21 19:37:54 -0400

    Simple: Added second Dev Board build

---
+++ Simple Blink/MSP430FR2311/main.c
@@ -0,0 +1,17 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;               // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5; // Disable the GPIO power-on default high-impedance mode
+    P1DIR |= 0x01;                          //set P1.0 as an output. Use "|=" so that it does not overwrite unnecessary bits
+    volatile unsigned int i;                //This will not be optimized and removed by the compiler
+    while(1){                            //Infinite while loop
+        P1OUT ^= 0x01;                      //Flip P1OUT to be the opposite of what it was before
+        for(i = 0; i < 20000; i++);         //A for loop that counts up, basically a delay that occupies CPU cycles
+    }
+}
