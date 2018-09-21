019996e1fd2b204e3e35f604dbcc6fdd1330ba06
unknown <mclemoret0@students.rowan.edu>
2018-09-21 18:43:34 -0400

    BtnBlink: Added button functionality

--- Button Blink/main.c
+++ Button Blink/main.c
@@ -1,17 +1,20 @@
 #include <msp430.h> 
 
 
-#define Btn BIT3                 //Define "BUTTON" as bit 3.
+#define Btn BIT3                 //Define "Btn" as bit 3.
 #define LED BIT0                   //Define "LED0" as bit 0.
-#define PnB (P1IN & BUTTON) //Define "INP" for checking if there is an input on pin 1.3.
-
+#define PnB (P1IN & Btn)        //Define "INP" for checking if there is an input on pin 1.3.
+                                //P1IN is the address of the register where all port bits are read
+typedef int bool;
+#define true 1
+#define false 0
 /**
  * main.c
  */
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    int i = 0;
+    bool i = 0;
 
     P1SEL &= (~LED & ~Btn);
 
@@ -22,6 +25,16 @@ int main(void)
     P1REN |= Btn;
     P1OUT |= Btn;
 
+    while(1){
+        if(!PnB && !i){
+                    P1OUT ^= LED;
+                    i = 1;
+        }
+        else if(PnB && i){
+                   i = 0;
+       }
+
+    }
 
 
 }
